014c1af8dcd335a87c36fc6fa955778354d449c7
Azra-aa <azra.ahmad.algifari@gmail.com>
2024-07-21 17:49:15 +0700

    tambah filter by jenis faskes id 4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -129,6 +129,7 @@
                                   <option value="1" {{ request('jenis_faskes_id') == 1 ? 'selected' : '' }}>1</option>
                                   <option value="2" {{ request('jenis_faskes_id') == 2 ? 'selected' : '' }}>2</option>
                                   <option value="3" {{ request('jenis_faskes_id') == 3 ? 'selected' : '' }}>3</option>
+                                  <option value="4" {{ request('jenis_faskes_id') == 4 ? 'selected' : '' }}>4</option>
                               </select>
                           </div>
                       </form>
